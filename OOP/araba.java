1d25a590495ebda89c1b223f4a91c2042d09d3d4
Gürkan KIR <gurkan.kir@bil.omu.edu.tr>
2019-11-05 11:36:20 +0300

    Create araba.java

---
+++ OOP/araba.java
@@ -0,0 +1,61 @@
+
+public class Araba{
+
+    public String renk;
+    private int kapilar;
+    private int motor;
+    private String model;
+    private int tekerler;
+
+    public void setrenk(String renk){
+
+        this.renk = renk;
+    }
+
+    public String getrenk(){
+
+        return this.renk;
+    }
+
+    public void setkapilar(int kapilar){
+
+        this.kapilar = kapilar;
+    }
+
+    public int getkapilar(){
+
+        return this.kapilar;
+    }
+
+    public void setmotor(int motor){
+
+        this.motor = motor;
+    }
+
+    public int getmotor(){
+
+        return this.motor;
+    }
+
+    public void setmodel(String model){
+
+        this.model = model;
+    }
+
+    public String getmodel(){
+
+        return this.model;
+    }
+
+    public void settekerler(int tekerler){
+
+        this.tekerler = tekerler;
+    }
+
+    public int gettekerler(){
+
+        return this.tekerler;
+    }
+
+    
+}
